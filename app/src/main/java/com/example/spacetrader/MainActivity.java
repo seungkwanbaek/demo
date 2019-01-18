cd8070ecdd6b4d148ef5d00f1ec8c1abce49cc7c
Bryan Baek <seungkwan.baek@gmail.com>
2019-01-18 14:11:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/spacetrader/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.spacetrader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //asdfasdf
+    //asdasdf
+    //3fsdfsdf
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void doSomething() {
+        //asdfasdfasdf
+
+    }
+}
